65548a481fcb3e5d35398d1f13b964a55765aba6
naman_samra <naman@tartan.com>
2022-04-24 20:19:08 +0530

    ✨ description box

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,24 @@ function App() {
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
+        <div
+          style={{
+            display: 'flex',
+            backgroundColor: 'white',
+            borderRadius: '5px',
+            boxShadow: '8px 10px #e9e9e9',
+            minHeight: '400px',
+            flexDirection: 'column',
+            gap: '20px',
+            padding: 20,
+          }}
+        >
+          <div style={{ fontSize: '20px', color: '#344054' }}>Hello beta</div>
+          <div style={{ fontSize: '18px', color: '#344054' }}>Welcome Here</div>
+          <div style={{ fontSize: '24px', color: '#344054' }}>
+            This is one of the greatest counter
+          </div>
+        </div>
         <p>
           <a
             className="App-link"
