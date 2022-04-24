4d8d4191b978978cf67341b073db64922c766846
naman_samra <naman@tartan.com>
2022-04-25 00:47:32 +0530

    🔥 remove react with style div

--- src/App.jsx
+++ src/App.jsx
@@ -68,15 +68,6 @@ function App() {
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React with style
-          </a>
-          {' | '}
-          <a
-            className="App-link"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
